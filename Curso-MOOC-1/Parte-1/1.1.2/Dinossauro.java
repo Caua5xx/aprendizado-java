5ad446fad56ce455e4a1c3ceac37a0485b30d723
Caua5xx <cauaamaral32@gmail.com>
2026-01-08 22:26:24 -0300

    1.1.2 exercicios

---
+++ Curso-MOOC-1/Parte-1/1.1.2/Dinossauro.java
@@ -0,0 +1,7 @@
+public class Dinossauro {
+    public static void main(String[] args) {
+        System.out.println("Era uma vez");
+        System.out.println("Havia");
+        System.out.println("um dinossauro");
+    }
+}
---
+++ Curso-MOOC-1/Parte-1/1.1.2/EraUmaVez.java
@@ -0,0 +1,7 @@
+public class EraUmaVez {
+    public static void main(String[] args) {
+        System.out.println("Era uma vez");
+        System.out.println("Havia");
+        System.out.println("Um programa");
+    }
+}
